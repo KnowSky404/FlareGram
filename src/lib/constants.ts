ab75a47b7638e56699e4bc9521084295814d44dc
KnowSky404 <git@knowsky404.com>
2026-04-27 12:25:57 +0800

    feat: add runtime env and d1 schema

---
+++ src/lib/constants.ts
@@ -0,0 +1,16 @@
+export const SUPPORTED_MESSAGE_KEYS = [
+  "text",
+  "photo",
+  "document",
+  "voice",
+  "video",
+] as const;
+
+export const ADMIN_ROUTE_NOT_FOUND_MESSAGE =
+  "Cannot route this reply: original user mapping was not found.";
+
+export const ADMIN_DELIVERY_FAILED_MESSAGE =
+  "Reply could not be delivered to the user.";
+
+export const UNSUPPORTED_MESSAGE_NOTICE =
+  "This message type is not supported in the current MVP.";
